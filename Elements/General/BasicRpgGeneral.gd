335d6639d2e35b12a2f2a40939217f0e5d13351f
JoshuahMi <38832572+JoshuahMi@users.noreply.github.com>
2026-07-08 22:37:45 +0200

    Added test level

--- Elements/General/BasicRpgGeneral.gd
+++ Elements/General/BasicRpgGeneral.gd
@@ -15,18 +15,4 @@ enum PlayerMovementStates {
 	ON_WALL_LADDER,			# While on a ladder.
 	ON_WALL_NORMAL,			# Walljump / wallrun
 	
-	
-	
-	
-	
-	
 }
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
